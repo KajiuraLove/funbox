a0a60e9f21f17478d8312ab2a06c7d991392a0c9
kajiura.eth <122961363+KajiuraLove@users.noreply.github.com>
2024-04-16 16:54:20 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+import CardSection from "./CardSection";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <CardSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
